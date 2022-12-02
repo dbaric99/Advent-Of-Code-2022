2731f1d02511d43e09f93b7f27147fdeaa2db9ce
dbaric99 <damjana.baric1999@gmail.com>
2022-12-02 07:35:39 +0100

    Day 2 - one star

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,7 +1,52 @@
-﻿var testInputPath = "../../../testInput.txt";
-var inputPath = "../../../input.txt";
+﻿using System.Drawing;
 
-var text = File.ReadAllText(inputPath);
+public enum opponent
+{
+    A = 1, //Rock
+    B = 2, //Paper
+    C = 3//Scissors
+    
+}
+
+public enum move
+{
+    X = 1, //Rock
+    Y = 2, //Paper
+    Z = 3, //Scissors
+}
+
+public class Day2
+{
+    public static void Main()
+    {
+        var testInputPath = "../../../testInput.txt";
+        var inputPath = "../../../input.txt";
+
+        var text = File.ReadAllText(inputPath);
+
+        var strategyGuide = text.Split("\n");
 
 
-Console.ReadKey();
+        int score = 0;
+        foreach(var res in strategyGuide)
+        {
+            var oneRound = res.Split(" ");
+
+            int opponentChoice = (int)Enum.Parse(typeof(opponent), oneRound[0]);
+            int yourChoice = (int)Enum.Parse(typeof(move), oneRound[1]);
+
+            score += yourChoice;
+
+            if (opponentChoice == yourChoice)
+                score += 3;
+            else if ((yourChoice == 1 && opponentChoice == 3) || (yourChoice == 2 && opponentChoice == 1) || (yourChoice == 3 && opponentChoice == 2))
+            {
+                score += 6;
+            }
+        }
+
+        Console.WriteLine(score);
+
+        Console.ReadKey();
+    }
+}
