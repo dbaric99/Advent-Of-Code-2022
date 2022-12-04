e06d37b583976a531d6419d8308efda55196f74e
dbaric99 <damjana.baric1999@gmail.com>
2022-12-04 06:20:19 +0100

    Day 4 - one star

---
+++ Day4/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Day3
+{
+    public static void Main()
+    {
+        var testInputPath = "../../../testInput.txt";
+        var inputPath = "../../../input.txt";
+
+        var text = File.ReadAllText(inputPath);
+
+        var pairs = text.Split("\n");
+
+        int counter = 0;
+
+        foreach (var p in pairs)
+        {
+            var pair = p.Split(",");
+
+            var firstElfRange = pair[0].Split("-").Select(Int32.Parse).ToArray();
+            var secondElfRange = pair[1].Split("-").Select(Int32.Parse).ToArray();
+
+            if ((firstElfRange[0] >= secondElfRange[0] && firstElfRange[1] <= secondElfRange[1]) || (secondElfRange[0] >= firstElfRange[0] && secondElfRange[1] <= firstElfRange[1]))
+            {
+                counter++;
+            }
+        }
+
+        Console.WriteLine("COUNTER: " + counter);
+
+        Console.ReadKey();
+    }
+}
