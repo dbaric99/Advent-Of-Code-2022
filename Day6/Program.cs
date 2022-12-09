999fd9b9de9f0220fa17fa2c0bdb9375359dbc8d
dbaric99 <damjana.baric1999@gmail.com>
2022-12-09 15:02:05 +0100

    Day7 - one star

---
+++ Day6/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Day6
+{
+    public static void Main()
+    {
+        var testInputPath = "../../../testInput.txt";
+        var inputPath = "../../../input.txt";
+
+        var text = File.ReadAllText(inputPath);
+
+        const int SIGNAL = 4;
+
+        for (int i = 0; i < text.Length - SIGNAL; i++)
+        {
+            var past = text.Substring(i, SIGNAL);
+            if (past.Length == past.Distinct().Count())
+            {
+                Console.WriteLine(i+SIGNAL);
+                break;
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
