d206de68e247e93da12c99e3b5aacdad2078da7e
dbaric99 <damjana.baric1999@gmail.com>
2022-12-12 21:30:15 +0100

    Day 12 - one star

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -5,55 +5,100 @@
         var testInputPath = "../../../testInput.txt";
         var inputPath = "../../../input.txt";
 
-        var text = File.ReadAllLines(testInputPath);
+        var text = File.ReadAllLines(inputPath);
 
         var inputGrid = text.Select(line => line.ToCharArray()).ToArray();
 
-        var length = inputGrid.Count(); //x
-        var height = inputGrid.First().Count(); //y
+        var n = inputGrid.Length;
+        var m = inputGrid[0].Length;
 
-        int startX = 0, startY = 0, endX = 0, endY = 0;
+        var start = (-1, -1);
+        var end = (-1, -1);
 
-        for(var i = 0; i < length; i++)
+        for (int i = 0; i < n; i++)
         {
-            for (var j = 0; j < height; j++)
+            for (int j = 0; j < m; j++)
             {
-                if (inputGrid[i][j] == 'S')
+                char c = inputGrid[i][j];
+                if (c == 'S')
                 {
-                    startX = i;
-                    startY = j;
+                    start = (i, j);
                 }
-                else if (inputGrid[i][j] == 'E')
+                if (c == 'E')
                 {
-                    endX = i;
-                    endY = j;
+                    end = (i, j);
                 }
             }
         }
 
-        //TODO check if in bounds
-        (int, int) ExploreNeighbours(int x, int y)
+        var visited = new int[n, m];
+
+        var elements = new List<(int, int, int)>
         {
-            var validMovements = new (int X, int Y)[] { (1, 0), (-1, 0), (0, 1), (0, -1) };
+            (0, start.Item1, start.Item2)
+        };
 
-            foreach (var item in validMovements)
+
+        while (true)
+        {
+            var tuple = elements.ElementAt(0);
+            elements.RemoveAt(0);
+            int steps = tuple.Item1;
+            int i = tuple.Item2;
+            int j = tuple.Item3;
+
+            if (visited[i,j] == 1)
             {
-                var newX = x + item.X;
-                var newY = y + item.Y;
-
-                if (GetHeightOfCharacter(inputGrid[newX][newY]) <= GetHeightOfCharacter(inputGrid[x][y]) + 1)
-                    return (newX, newY);
+                continue;
             }
-            return (-1, -1);
+            visited[i,j] = 1;
+
+            if ((i, j) == end)
+            {
+                Console.WriteLine("STEPS: " + steps);
+                break;
+            }
+
+            foreach ((int, int) coord in ExploreNeighbors(i, j))
+            {
+                int ii = coord.Item1;
+                int jj = coord.Item2;
+                elements.Add((steps + 1, ii, jj));
+            }
         }
 
-        int GetHeightOfCharacter(char c)
+        int Height(char c)
         {
             if (c == 'S') return 0;
             else if (c == 'E') return 25;
             else return (int)c - 97;
         }
 
+        IEnumerable<(int, int)> ExploreNeighbors(int x, int y)
+        {
+            var directions = new (int X, int Y)[4] { (1, 0), (-1, 0), (0, 1), (0, -1) };
+
+            foreach (var direction in directions)
+            {
+                int newX = x + direction.X;
+                int newY = y + direction.Y;
+
+                if (!((0 <= newX && newX < n) && (0 <= newY && newY < m)))
+                    continue;
+
+                if (Height(inputGrid[newX][newY]) <= Height(inputGrid[x][y]) + 1)
+                    yield return (newX, newY);
+            }
+        }
+
+
         Console.ReadLine();
     }
+}
+
+public class Node
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public char Elevation { get; set; }
 }
