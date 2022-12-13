50a8c1612e811f87b2bb45e42bedc3991d36801e
dbaric99 <damjana.baric1999@gmail.com>
2022-12-13 17:36:09 +0100

    Day 13 - trying to implement JsonNode to find if the variable is a value or an array

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Cryptography;
+﻿using System.Collections;
+using System.Security.Cryptography;
+using System.Text.Json.Nodes;
 
 public class Day7
 {
@@ -7,19 +9,26 @@ public class Day7
         var testInputPath = "../../../testInput.txt";
         var inputPath = "../../../input.txt";
 
-        var text = File.ReadAllLines(testInputPath).Where(line => !string.IsNullOrEmpty(line));
+        var text = File.ReadAllLines(inputPath).Where(line => !string.IsNullOrEmpty(line));
 
         var allPairs = new List<Pair>();
-
+        var pairCounter = 1;
         for (int i = 0; i < text.Count(); i+=2)
         {
-            allPairs.Add(new Pair(text.ElementAt(i), text.ElementAt(i + 1)));
+            allPairs.Add(new Pair(text.ElementAt(i), text.ElementAt(i + 1), pairCounter));
+            
+            pairCounter++;
         }
+
+        var counter = 0;
 
         foreach (var item in allPairs)
         {
-            Console.WriteLine($"LEFT: {item.Left}\tRIGHT: {item.Right}");
+            if (Pair.Compare(item) > 0)
+                counter += (allPairs.IndexOf(item) + 1);
         }
+
+        Console.WriteLine("ITEMS IN ORDER: " + counter);
 
         Console.ReadLine();
     }
@@ -27,12 +36,63 @@ public class Day7
 
 public class Pair
 {
-    public string Left { get; set; }
-    public string Right { get; set; }
+
+    public JsonNode Left { get; set; }
+    public JsonNode Right { get; set; }
+    public int Index { get; set; } = 0;
 
     public Pair(string left, string right)
     {
-        Left = left.Substring(1, left.Length - 2);
-        Right = right.Substring(1, right.Length - 2);
+        Left = JsonValue.Parse(left);
+        Right = JsonNode.Parse(right);
+    }
+
+    public Pair(string left, string right, int index)
+    {
+        Left = JsonValue.Parse(left);
+        Right = JsonNode.Parse(right);
+        Index = index;
+    }
+
+    public static int Compare(Pair pairToCompare)
+    {
+        var left = pairToCompare.Left;
+        var right = pairToCompare.Right;
+
+        if (left is JsonValue && right is JsonValue)
+        {
+            return (int)left - (int)right;
+        }
+        else
+        {
+            var leftArr = left as JsonArray;
+            var rightArr = right as JsonArray;
+
+            if (left is not JsonArray)
+                leftArr = new JsonArray((int)left);
+            if (right is not JsonArray)
+                rightArr = new JsonArray((int)right);
+
+
+            var result = 0;
+            var i = 0;
+            foreach (var l in leftArr)
+            {
+                if (i >= 0 && i < rightArr.Count())
+                {
+                    var r = rightArr[i];
+                    result = Compare(new Pair(l.ToString(), r.ToString()));
+                    if (result != 0)
+                    {
+                        break;
+                    }
+                }
+                i++;
+            }
+
+
+
+            return result == 0 ? leftArr.Count - rightArr.Count : pairToCompare.Index;
+        }
     }
 }
