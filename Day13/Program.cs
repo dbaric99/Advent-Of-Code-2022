80c989ffd3c05afc5855e5cfa0e8b95b739bb9cd
dbaric99 <damjana.baric1999@gmail.com>
2022-12-13 11:39:00 +0100

    Day 13 - reading lines from file and saving them to a list

---
+++ Day13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+
+public class Day7
+{
+    public static void Main()
+    {
+        var testInputPath = "../../../testInput.txt";
+        var inputPath = "../../../input.txt";
+
+        var text = File.ReadAllLines(testInputPath).Where(line => !string.IsNullOrEmpty(line));
+
+        var allPairs = new List<Pair>();
+
+        for (int i = 0; i < text.Count(); i+=2)
+        {
+            allPairs.Add(new Pair(text.ElementAt(i), text.ElementAt(i + 1)));
+        }
+
+        foreach (var item in allPairs)
+        {
+            Console.WriteLine($"LEFT: {item.Left}\tRIGHT: {item.Right}");
+        }
+
+        Console.ReadLine();
+    }
+}
+
+public class Pair
+{
+    public string Left { get; set; }
+    public string Right { get; set; }
+
+    public Pair(string left, string right)
+    {
+        Left = left.Substring(1, left.Length - 2);
+        Right = right.Substring(1, right.Length - 2);
+    }
+}
