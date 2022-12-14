63c31399f67523f15b705df9cf9035b891a79942
dbaric99 <damjana.baric1999@gmail.com>
2022-12-14 01:00:27 +0100

    Day 13 - two stars (wrote custom function for comparison)

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -9,30 +9,88 @@ public class Day7
         var testInputPath = "../../../testInput.txt";
         var inputPath = "../../../input.txt";
 
-        var text = File.ReadAllLines(inputPath).Where(line => !string.IsNullOrEmpty(line));
+        var text = File.ReadAllLines(inputPath).Where(line => !string.IsNullOrEmpty(line)).ToList();
 
-        var allPairs = new List<Pair>();
-        var pairCounter = 1;
-        for (int i = 0; i < text.Count(); i+=2)
+        text.AddRange(new string[] { "[[2]]", "[[6]]" });
+
+        var start = "[[2]]";
+        var end = "[[6]]";
+
+        var jsonList = new List<JsonNode>();
+
+        foreach (var item in text)
         {
-            allPairs.Add(new Pair(text.ElementAt(i), text.ElementAt(i + 1), pairCounter));
-            
-            pairCounter++;
+            jsonList.Add(JsonValue.Parse(item));
         }
 
-        var sum = 0;
+        jsonList.Sort(Compare);
+        var startInd = 0;
+        var endInd = 0;
         var counter = 1;
-        foreach (var item in allPairs)
+        foreach (var item in jsonList)
         {
-            if (Pair.Compare(item) < 0)
-                sum += counter;
-
+            if (item.ToJsonString() == start)
+                startInd = counter;
+            else if (item.ToJsonString() == end)
+                endInd = counter;
             counter++;
         }
 
-        Console.WriteLine("ITEMS IN ORDER: " + sum); //6623
+        Console.WriteLine(startInd + "\t" + endInd + "\t" + (startInd*endInd));
 
-        Console.ReadLine();
+        int Compare(JsonNode left, JsonNode right)
+        {
+            if (left is JsonValue && right is JsonValue)
+            {
+                return Math.Sign((int)left - (int)right);
+            }
+            else
+            {
+                var leftArr = left as JsonArray;
+                var rightArr = right as JsonArray;
+
+                if (left is not JsonArray)
+                    leftArr = new JsonArray((int)left);
+                if (right is not JsonArray)
+                    rightArr = new JsonArray((int)right);
+
+                for (int i = 0; i < leftArr.Count && i < rightArr.Count; i++)
+                {
+                    int comparison = Compare(leftArr[i], rightArr[i]);
+                    if (comparison != 0)
+                    {
+                        return Math.Sign(comparison);
+                    }
+                }
+                return Math.Sign(leftArr.Count - rightArr.Count);
+            }
+        }
+
+            //var allPairs = new List<Pair>();
+            //var pairCounter = 1;
+            //for (int i = 0; i < text.Count(); i+=2)
+            //{
+            //    allPairs.Add(new Pair(text.ElementAt(i), text.ElementAt(i + 1), pairCounter));
+
+            //    pairCounter++;
+            //}
+
+            //var sum = 0;
+            //var counter = 1;
+            //foreach (var item in allPairs)
+            //{
+            //    if (Pair.Compare(item) < 0)
+            //        sum += counter;
+
+            //    counter++;
+            //}
+
+            //Console.WriteLine("ITEMS IN ORDER: " + sum);
+
+            // Define a custom comparer that compares the values of two JSON nodes
+
+
+            Console.ReadLine();
     }
 }
 
